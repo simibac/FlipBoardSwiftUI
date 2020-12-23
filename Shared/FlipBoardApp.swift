121581cd5d9127e37f5e176afc3d997e977470af
Simon <simibac93@gmail.com>
2020-12-23 16:29:26 +0100

    Initial Commit

---
+++ Shared/FlipBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipBoardApp.swift
+//  Shared
+//
+//  Created by Simon Bachmann on 23.12.20.
+//
+
+import SwiftUI
+
+@main
+struct FlipBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
